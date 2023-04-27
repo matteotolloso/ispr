964c835fe2daef09c67f89981d6e3b15b90533e6
matteotolloso <matteotolloso@gmail.com>
2023-04-27 21:28:02 +0200

    structure

---
+++ midterm_3/generate.py
@@ -0,0 +1,48 @@
+# https://github.com/spro/char-rnn.pytorch
+
+import torch
+from helpers import *
+from model import *
+
+def _generate(decoder, prime_str, predict_len, temperature, cuda):
+    hidden = decoder.init_hidden(1)
+    prime_input = Variable(char_tensor(prime_str).unsqueeze(0))
+
+    if cuda:
+        hidden = hidden.cuda()
+        prime_input = prime_input.cuda()
+    predicted = prime_str
+
+    # Use priming string to "build up" hidden state
+    for p in range(len(prime_str) - 1):
+        _, hidden = decoder(prime_input[:,p], hidden)
+        
+    inp = prime_input[:,-1]
+    
+    for p in range(predict_len):
+        output, hidden = decoder(inp, hidden)
+        
+        # Sample from the network as a multinomial distribution
+        output_dist = output.data.view(-1).div(temperature).exp()
+        top_i = torch.multinomial(output_dist, 1)[0]
+
+        # Add predicted character to string and use as next input
+        predicted_char = all_characters[top_i]
+        predicted += predicted_char
+        inp = Variable(char_tensor(predicted_char).unsqueeze(0))
+        if cuda:
+            inp = inp.cuda()
+
+    return predicted
+
+# Run as standalone script
+def generate(filename : str, temperature : float):
+
+    prime_str = '^'
+    cuda = True
+
+    decoder = torch.load(filename,  map_location=torch.device('cpu'))
+    print(_generate(decoder, prime_str=prime_str, predict_len=168, temperature=temperature, cuda = cuda ))
+
+if __name__ == '__main__':
+    generate('lercio_padded.pt', 0.8)
--- midterm_3/main.py
+++ midterm_3/main.py
@@ -1,19 +1,3 @@
-import os
+from charnn.generate import generate
 
-path = "./midterm_3/lercio_headlines.txt"
-
-options =\
-"--model            gru   " +\
-"--n_epochs         700    " +\
-"--print_every      50   " +\
-"--hidden_size      32    " +\
-"--n_layers         4     " +\
-"--learning_rate    0.01  " +\
-"--chunk_len        100   " +\
-"--batch_size       1000   " +\
-"--cuda                   " 
-
-command = f"python ./midterm_3/char-rnn-pytorch/train.py {path} {options}"
-
-
-os.system(command)
+generate('lercio_padded.pt', 0.8)
